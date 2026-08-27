6cf9525a76260370f79fa2203c84f419dea6954e
Colton Dukes <coltondukes@gmail.com>
2026-08-26 19:30:54 -0500

    Fix: Clean functions.php - remove problematic code, restore working version

--- wp-content/themes/dtc-southwest-child/functions.php
+++ wp-content/themes/dtc-southwest-child/functions.php
@@ -8,12 +8,26 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 } );
 
-// Enqueue custom interactive script
-add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_script( 'dtc-interactive', get_stylesheet_directory_uri() . '/assets/js/interactive.js', array(), '1.0', true );
+// Enable WooCommerce support
+add_theme_support( 'woocommerce' );
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+
+// Set products per page
+add_filter( 'loop_shop_per_page', function() {
+    return 12;
 } );
 
-// Add hamburger menu toggle script
+// Remove default WooCommerce styles
+add_filter( 'woocommerce_enqueue_styles', function( $enqueue_styles ) {
+    unset( $enqueue_styles['woocommerce-general'] );
+    unset( $enqueue_styles['woocommerce-layout'] );
+    unset( $enqueue_styles['woocommerce-smallscreen'] );
+    return $enqueue_styles;
+}, 999 );
+
+// Hamburger menu toggle script
 add_action( 'wp_footer', function() {
     ?>
     <script>
@@ -40,27 +54,8 @@ add_action( 'wp_footer', function() {
     <?php
 } );
 
-// WooCommerce Support
-add_theme_support( 'woocommerce' );
-add_theme_support( 'wc-product-gallery-zoom' );
-add_theme_support( 'wc-product-gallery-lightbox' );
-add_theme_support( 'wc-product-gallery-slider' );
-
-// Products per page
-add_filter( 'loop_shop_per_page', function() { return 12; } );
-
-// Remove default WooCommerce styles
-add_filter( 'woocommerce_enqueue_styles', function( $enqueue_styles ) {
-    unset( $enqueue_styles['woocommerce-general'] );
-    unset( $enqueue_styles['woocommerce-layout'] );
-    unset( $enqueue_styles['woocommerce-smallscreen'] );
-    return $enqueue_styles;
-}, 999 );
-
-// Related products
-add_filter( 'woocommerce_output_related_products_args', function( $args ) {
-    $args['posts_per_page'] = 3;
-    $args['columns'] = 3;
-    return $args;
+// Enqueue interactive script
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_script( 'dtc-interactive', get_stylesheet_directory_uri() . '/assets/js/interactive.js', array(), '1.0', true );
 } );
 ?>
